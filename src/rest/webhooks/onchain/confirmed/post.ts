6fc2d1fc5df8f6da996c826555cfae78f2ffffa4
Agustin Kassis <webmaster@masize.com>
2024-04-14 23:51:03 -0300

    updated confirmation endpoint

--- src/rest/webhooks/onchain/confirmed/post.ts
+++ src/rest/webhooks/onchain/confirmed/post.ts
@@ -2,45 +2,52 @@
 
 import { ExtendedRequest, requiredEnvVar } from '@lawallet/module';
 import { GameContext } from '@src/index';
-import { CryptoapisConfirmedTx } from '@src/types/cryptoapis';
-import { verifySignature } from '@src/utils';
+import { BlockCypherNotification } from '@src/types/blockcypher';
 import type { Response } from 'express';
+
+interface AddressTxOutput {
+  txid: string;
+  address: string;
+  vout: number;
+  amount: number;
+}
 
 // eslint-disable-next-line @typescript-eslint/require-await
 async function handler<Context extends GameContext>(
   req: ExtendedRequest<Context>,
   res: Response,
 ) {
-  const confirmedTx = req.body as CryptoapisConfirmedTx;
-  console.info('--- POST confirmed---');
-  console.dir(confirmedTx);
+  const confirmedTx = req.body as BlockCypherNotification;
 
-  // Check if the transaction is outgoing
-  if (confirmedTx.data.item.direction === 'outgoing') {
-    console.info('Outgoing transaction');
-    return res.status(200).send('OK');
+  // Verify Blockcypher x-eventid
+  if (requiredEnvVar('BLOCKCYPHER_X_EVENTID') !== req.headers['x-eventid']) {
+    return res.status(401).send('Unauthorized');
   }
 
-  // Verify the signature
-  if (
-    !verifySignature(
-      req.body,
-      requiredEnvVar('CRYPTOAPIS_LOCAL_SECRET'),
-      req.headers['x-signature'] as string,
-    )
-  ) {
-    console.error('Invalid signature');
-    return res.status(400).send('Invalid signature');
-  }
+  const addressTxOutputs: AddressTxOutput[] = confirmedTx.outputs.map(
+    (output, k) => {
+      return {
+        txid: confirmedTx.hash,
+        address: output.addresses[0] as string,
+        vout: k,
+        amount: output.value,
+      };
+    },
+  );
 
-  const txId = confirmedTx.data.item.transactionId;
-  const address = confirmedTx.data.item.address;
+  console.dir('addressTxOutputs: ');
+  console.dir(addressTxOutputs);
 
-  console.info('Transaction info');
-  console.dir({
-    txId,
-    address,
-  });
+  // Iterate through each address
+  // addressTxOutputs
+  // Look for txid in db
+  // If exists
+  // If confirmed. Add Power to user and update Tx
+
+  // If doesnt exist
+  // Look for address in db
+  // Add Power to user and update Tx
+
   return res.status(200).send('OK');
 }
 
