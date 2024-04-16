435c7e34ad5dfac6d59c143f9cea1489f5e97155
Juan José Ramírez <artjjrn@gmail.com>
2024-04-16 13:32:22 -0300

    Output logs on freeze

--- src/lib/freeze.ts
+++ src/lib/freeze.ts
@@ -1,9 +1,12 @@
-import { Kind, nowInSeconds, requiredEnvVar } from '@lawallet/module';
+import { Kind, logger, nowInSeconds, requiredEnvVar } from '@lawallet/module';
 import { NostrEvent } from '@nostr-dev-kit/ndk';
 import { GAME_STATE_SELECT, GameStateData, powerByPlayer } from '@src/utils';
 import { GameContext } from '@src/index';
 import { Status } from '@prisma/client';
 import { UnsignedEvent, getEventHash } from 'nostr-tools';
+import { Debugger } from 'debug';
+
+const log: Debugger = logger.extend('lib:freeze');
 
 export function freezeEvent(
   game: Pick<GameStateData, 'id' | 'currentBlock' | 'currentRound'>,
@@ -33,6 +36,7 @@ export async function freezeGame(
   gameId: string,
   ctx: GameContext,
 ): Promise<void> {
+  log('Freezing game %s', gameId);
   const updatedGame = await ctx.prisma.game.update({
     select: GAME_STATE_SELECT,
     data: { status: Status.FREEZE },
@@ -41,4 +45,5 @@ export async function freezeGame(
   const freezeE = freezeEvent(updatedGame);
   ctx.statePublisher.queue(gameId, getEventHash(freezeE as UnsignedEvent));
   await ctx.outbox.publish(freezeE);
+  log('Froze game %s', gameId);
 }
